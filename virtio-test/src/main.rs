d0c56e717b20509b98784b348e6451379762a694
luojia65 <me@luojia.cc>
2021-05-18 04:37:45 +0800

    Verify header magic

--- virtio-test/src/main.rs
+++ virtio-test/src/main.rs
@@ -3,6 +3,182 @@
 #![no_std]
 #![no_main]
 
+mod mmio;
+
+use linked_list_allocator::LockedHeap;
+
+use core::mem::MaybeUninit;
+const KERNEL_HEAP_SIZE: usize = 64 * 1024;
+static mut HEAP_SPACE: MaybeUninit<[u8; KERNEL_HEAP_SIZE]> = MaybeUninit::uninit();
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+unsafe fn init_heap() {
+    ALLOCATOR.lock().init(
+        HEAP_SPACE.as_ptr() as usize, KERNEL_HEAP_SIZE
+    )
+}
+
+#[allow(unused)]
+#[cfg_attr(not(test), alloc_error_handler)]
+fn oom(layout: core::alloc::Layout) -> ! {
+    println!("!! Out of memory: {:?}", layout);
+    println!("!! Kernel: Test failed due to out of memory");
+    sbi::shutdown()
+}
+
+use riscv::register::stvec::{self, TrapMode};
+
+pub extern "C" fn rust_main(hartid: usize, dtb_pa: usize) -> ! {
+    extern "C" { fn sbss(); fn ebss();/* fn ekernel(); */}
+    unsafe { r0::zero_bss(&mut sbss as *mut _ as *mut u64, &mut ebss as *mut _ as *mut u64) };
+    println!("<< Kernel: Hart id = {}, DTB physical address = {:#x}", hartid, dtb_pa);
+    unsafe { init_heap() };
+    unsafe { stvec::write(start_trap as usize, TrapMode::Direct) };
+    
+    // unsafe { dump_dtb(dtb_pa) };
+
+    
+    let header = unsafe {
+        &mut *(0x10001000 as *mut mmio::VirtIoHeader)
+    };
+    println!("Magic = {:#x}, Version = {}", header.magic.read(), header.version.read());
+
+    println!("<< Kernel: test SUCCESS, shutdown");
+    sbi::shutdown()
+}
+
+// unsafe fn dump_dtb(dtb_pa: usize) {
+//     const DEVICE_TREE_MAGIC: u32 = 0xD00DFEED;
+//     #[repr(C)]
+//     struct DtbHeader { magic: u32, size: u32 }
+//     let header = &*(dtb_pa as *const DtbHeader);
+//     let magic = u32::from_be(header.magic);
+//     if magic == DEVICE_TREE_MAGIC {
+//         let size = u32::from_be(header.size);
+//         // 拷贝数据，加载并遍历
+//         let data = core::slice::from_raw_parts(dtb_pa as *const u8, size as usize);
+//         if let Ok(dt) = device_tree::DeviceTree::load(data) {
+//             println!("{:?}", dt);
+//         }
+//     }
+// }
+
+pub extern "C" fn rust_trap_exception() {
+
+}
+
+use core::panic::PanicInfo;
+
+#[cfg_attr(not(test), panic_handler)]
+#[allow(unused)]
+fn panic(info: &PanicInfo) -> ! {
+    println!("!! Kernel: {}", info);
+    println!("!! Kernel: Test failed due to panic");
+    sbi::shutdown()
+}
+
+const BOOT_STACK_SIZE: usize = 4096 * 4 * 8;
+
+static mut BOOT_STACK: [u8; BOOT_STACK_SIZE] = [0; BOOT_STACK_SIZE];
+
+#[naked]
+#[link_section = ".text.entry"] 
+#[export_name = "_start"]
+unsafe extern "C" fn entry() -> ! {
+    asm!("
+    # 1. set sp
+    # sp = bootstack + (hartid + 1) * 0x10000
+    add     t0, a0, 1
+    slli    t0, t0, 14
+1:  auipc   sp, %pcrel_hi({boot_stack})
+    addi    sp, sp, %pcrel_lo(1b)
+    add     sp, sp, t0
+    # 2. jump to rust_main (absolute address)
+1:  auipc   t0, %pcrel_hi({rust_main})
+    addi    t0, t0, %pcrel_lo(1b)
+    jr      t0
+    ", 
+    boot_stack = sym BOOT_STACK, 
+    rust_main = sym rust_main,
+    options(noreturn))
+}
+
+#[cfg(target_pointer_width = "64")]
+macro_rules! define_store_load {
+    () => {
+        ".altmacro
+        .macro STORE reg, offset
+            sd  \\reg, \\offset* {REGBYTES} (sp)
+        .endm
+        .macro LOAD reg, offset
+            ld  \\reg, \\offset* {REGBYTES} (sp)
+        .endm"
+    };
+}
+
+#[cfg(target_pointer_width = "32")]
+macro_rules! define_store_load {
+    () => {
+        ".altmacro
+        .macro STORE reg, offset
+            sw  \\reg, \\offset* {REGBYTES} (sp)
+        .endm
+        .macro LOAD reg, offset
+            lw  \\reg, \\offset* {REGBYTES} (sp)
+        .endm"
+    };
+}
+
+#[naked]
+#[link_section = ".text"]
+unsafe extern "C" fn start_trap() {
+    asm!(define_store_load!(), "
+    .p2align 2
+    addi    sp, sp, -16 * {REGBYTES}
+    STORE   ra, 0
+    STORE   t0, 1
+    STORE   t1, 2
+    STORE   t2, 3
+    STORE   t3, 4
+    STORE   t4, 5
+    STORE   t5, 6
+    STORE   t6, 7
+    STORE   a0, 8
+    STORE   a1, 9
+    STORE   a2, 10
+    STORE   a3, 11
+    STORE   a4, 12
+    STORE   a5, 13
+    STORE   a6, 14
+    STORE   a7, 15
+    mv      a0, sp
+    call    {rust_trap_exception}
+    LOAD    ra, 0
+    LOAD    t0, 1
+    LOAD    t1, 2
+    LOAD    t2, 3
+    LOAD    t3, 4
+    LOAD    t4, 5
+    LOAD    t5, 6
+    LOAD    t6, 7
+    LOAD    a0, 8
+    LOAD    a1, 9
+    LOAD    a2, 10
+    LOAD    a3, 11
+    LOAD    a4, 12
+    LOAD    a5, 13
+    LOAD    a6, 14
+    LOAD    a7, 15
+    addi    sp, sp, 16 * {REGBYTES}
+    sret
+    ",
+    REGBYTES = const core::mem::size_of::<usize>(),
+    rust_trap_exception = sym rust_trap_exception,
+    options(noreturn))
+}
+
 #[macro_use]
 mod console {
     use super::sbi::*;
@@ -172,170 +348,4 @@ mod sbi {
     pub fn set_timer(time: usize) {
         sbi_call_legacy(SBI_SET_TIMER, time, 0, 0);
     }
-}
-
-use linked_list_allocator::LockedHeap;
-
-use core::mem::MaybeUninit;
-const KERNEL_HEAP_SIZE: usize = 64 * 1024;
-static mut HEAP_SPACE: MaybeUninit<[u8; KERNEL_HEAP_SIZE]> = MaybeUninit::uninit();
-
-#[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
-
-unsafe fn init_heap() {
-    ALLOCATOR.lock().init(
-        HEAP_SPACE.as_ptr() as usize, KERNEL_HEAP_SIZE
-    )
-}
-
-#[allow(unused)]
-#[cfg_attr(not(test), alloc_error_handler)]
-fn oom(_layout: core::alloc::Layout) -> ! {
-    loop {}
-}
-
-use riscv::register::stvec::{self, TrapMode};
-
-pub extern "C" fn rust_main(hartid: usize, dtb_pa: usize) -> ! {
-    extern "C" { fn sbss(); fn ebss();/* fn ekernel(); */}
-    unsafe { r0::zero_bss(&mut sbss as *mut _ as *mut u64, &mut ebss as *mut _ as *mut u64) };
-    println!("<< Kernel: Hart id = {}, DTB physical address = {:#x}", hartid, dtb_pa);
-    unsafe { init_heap() };
-    unsafe { stvec::write(start_trap as usize, TrapMode::Direct) };
-    
-    unsafe { dump_dtb(dtb_pa) };
-
-    println!("<< Kernel: test SUCCESS, shutdown");
-    sbi::shutdown()
-}
-
-unsafe fn dump_dtb(dtb_pa: usize) {
-    const DEVICE_TREE_MAGIC: u32 = 0xD00DFEED;
-    #[repr(C)]
-    struct DtbHeader { magic: u32, size: u32 }
-    let header = &*(dtb_pa as *const DtbHeader);
-    let magic = u32::from_be(header.magic);
-    if magic == DEVICE_TREE_MAGIC {
-        let size = u32::from_be(header.size);
-        // 拷贝数据，加载并遍历
-        let data = core::slice::from_raw_parts(dtb_pa as *const u8, size as usize);
-        if let Ok(dt) = device_tree::DeviceTree::load(data) {
-            println!("{:?}", dt);
-        }
-    }
-}
-
-pub extern "C" fn rust_trap_exception() {
-
-}
-
-use core::panic::PanicInfo;
-
-#[cfg_attr(not(test), panic_handler)]
-#[allow(unused)]
-fn panic(info: &PanicInfo) -> ! {
-    println!("!! Kernel: {}", info);
-    println!("!! Kernel: Test failed due to panic");
-    sbi::shutdown()
-}
-
-const BOOT_STACK_SIZE: usize = 4096 * 4 * 8;
-
-static mut BOOT_STACK: [u8; BOOT_STACK_SIZE] = [0; BOOT_STACK_SIZE];
-
-#[naked]
-#[link_section = ".text.entry"] 
-#[export_name = "_start"]
-unsafe extern "C" fn entry() -> ! {
-    asm!("
-    # 1. set sp
-    # sp = bootstack + (hartid + 1) * 0x10000
-    add     t0, a0, 1
-    slli    t0, t0, 14
-1:  auipc   sp, %pcrel_hi({boot_stack})
-    addi    sp, sp, %pcrel_lo(1b)
-    add     sp, sp, t0
-    # 2. jump to rust_main (absolute address)
-1:  auipc   t0, %pcrel_hi({rust_main})
-    addi    t0, t0, %pcrel_lo(1b)
-    jr      t0
-    ", 
-    boot_stack = sym BOOT_STACK, 
-    rust_main = sym rust_main,
-    options(noreturn))
-}
-
-#[cfg(target_pointer_width = "64")]
-macro_rules! define_store_load {
-    () => {
-        ".altmacro
-        .macro STORE reg, offset
-            sd  \\reg, \\offset* {REGBYTES} (sp)
-        .endm
-        .macro LOAD reg, offset
-            ld  \\reg, \\offset* {REGBYTES} (sp)
-        .endm"
-    };
-}
-
-#[cfg(target_pointer_width = "32")]
-macro_rules! define_store_load {
-    () => {
-        ".altmacro
-        .macro STORE reg, offset
-            sw  \\reg, \\offset* {REGBYTES} (sp)
-        .endm
-        .macro LOAD reg, offset
-            lw  \\reg, \\offset* {REGBYTES} (sp)
-        .endm"
-    };
-}
-
-#[naked]
-#[link_section = ".text"]
-unsafe extern "C" fn start_trap() {
-    asm!(define_store_load!(), "
-    .p2align 2
-    addi    sp, sp, -16 * {REGBYTES}
-    STORE   ra, 0
-    STORE   t0, 1
-    STORE   t1, 2
-    STORE   t2, 3
-    STORE   t3, 4
-    STORE   t4, 5
-    STORE   t5, 6
-    STORE   t6, 7
-    STORE   a0, 8
-    STORE   a1, 9
-    STORE   a2, 10
-    STORE   a3, 11
-    STORE   a4, 12
-    STORE   a5, 13
-    STORE   a6, 14
-    STORE   a7, 15
-    mv      a0, sp
-    call    {rust_trap_exception}
-    LOAD    ra, 0
-    LOAD    t0, 1
-    LOAD    t1, 2
-    LOAD    t2, 3
-    LOAD    t3, 4
-    LOAD    t4, 5
-    LOAD    t5, 6
-    LOAD    t6, 7
-    LOAD    a0, 8
-    LOAD    a1, 9
-    LOAD    a2, 10
-    LOAD    a3, 11
-    LOAD    a4, 12
-    LOAD    a5, 13
-    LOAD    a6, 14
-    LOAD    a7, 15
-    addi    sp, sp, 16 * {REGBYTES}
-    sret
-    ",
-    REGBYTES = const core::mem::size_of::<usize>(),
-    rust_trap_exception = sym rust_trap_exception,
-    options(noreturn))
 }
---
+++ virtio-test/src/mmio.rs
@@ -0,0 +1,173 @@
+use volatile_register::{RO, WO, RW};
+
+/// MMIO Device Legacy Register Interface.
+///
+/// Ref: 4.2.4 Legacy interface
+#[repr(C)]
+pub struct VirtIoHeader {
+    /// Magic value
+    pub magic: RO<u32>,
+
+    /// Device version number
+    ///
+    /// Legacy device returns value 0x1.
+    pub version: RO<u32>,
+
+    /// Virtio Subsystem Device ID
+    device_id: RO<u32>,
+
+    /// Virtio Subsystem Vendor ID
+    vendor_id: RO<u32>,
+
+    /// Flags representing features the device supports
+    device_features: RO<u32>,
+
+    /// Device (host) features word selection
+    device_features_sel: WO<u32>,
+
+    /// Reserved
+    __r1: [u32; 2],
+
+    /// Flags representing device features understood and activated by the driver
+    driver_features: WO<u32>,
+
+    /// Activated (guest) features word selection
+    driver_features_sel: WO<u32>,
+
+    /// Guest page size
+    ///
+    /// The driver writes the guest page size in bytes to the register during
+    /// initialization, before any queues are used. This value should be a
+    /// power of 2 and is used by the device to calculate the Guest address
+    /// of the first queue page (see QueuePFN).
+    guest_page_size: WO<u32>,
+
+    /// Reserved
+    __r2: u32,
+
+    /// Virtual queue index
+    ///
+    /// Writing to this register selects the virtual queue that the following
+    /// operations on the QueueNumMax, QueueNum, QueueAlign and QueuePFN
+    /// registers apply to. The index number of the first queue is zero (0x0).
+    queue_sel: WO<u32>,
+
+    /// Maximum virtual queue size
+    ///
+    /// Reading from the register returns the maximum size of the queue the
+    /// device is ready to process or zero (0x0) if the queue is not available.
+    /// This applies to the queue selected by writing to QueueSel and is
+    /// allowed only when QueuePFN is set to zero (0x0), so when the queue is
+    /// not actively used.
+    queue_num_max: RO<u32>,
+
+    /// Virtual queue size
+    ///
+    /// Queue size is the number of elements in the queue. Writing to this
+    /// register notifies the device what size of the queue the driver will use.
+    /// This applies to the queue selected by writing to QueueSel.
+    queue_num: WO<u32>,
+
+    /// Used Ring alignment in the virtual queue
+    ///
+    /// Writing to this register notifies the device about alignment boundary
+    /// of the Used Ring in bytes. This value should be a power of 2 and
+    /// applies to the queue selected by writing to QueueSel.
+    queue_align: WO<u32>,
+
+    /// Guest physical page number of the virtual queue
+    ///
+    /// Writing to this register notifies the device about location of the
+    /// virtual queue in the Guest’s physical address space. This value is
+    /// the index number of a page starting with the queue Descriptor Table.
+    /// Value zero (0x0) means physical address zero (0x00000000) and is illegal.
+    /// When the driver stops using the queue it writes zero (0x0) to this
+    /// register. Reading from this register returns the currently used page
+    /// number of the queue, therefore a value other than zero (0x0) means that
+    /// the queue is in use. Both read and write accesses apply to the queue
+    /// selected by writing to QueueSel.
+    queue_pfn: RW<u32>,
+
+    /// new interface only
+    queue_ready: RW<u32>,
+
+    /// Reserved
+    __r3: [u32; 2],
+
+    /// Queue notifier
+    queue_notify: WO<u32>,
+
+    /// Reserved
+    __r4: [u32; 3],
+
+    /// Interrupt status
+    interrupt_status: RO<u32>,
+
+    /// Interrupt acknowledge
+    interrupt_ack: WO<u32>,
+
+    /// Reserved
+    __r5: [u32; 2],
+
+    /// Device status
+    ///
+    /// Reading from this register returns the current device status flags.
+    /// Writing non-zero values to this register sets the status flags,
+    /// indicating the OS/driver progress. Writing zero (0x0) to this register
+    /// triggers a device reset. The device sets QueuePFN to zero (0x0) for
+    /// all queues in the device. Also see 3.1 Device Initialization.
+    status: RW<DeviceStatus>,
+
+    /// Reserved
+    __r6: [u32; 3],
+
+    // new interface only since here
+    queue_desc_low: WO<u32>,
+    queue_desc_high: WO<u32>,
+
+    /// Reserved
+    __r7: [u32; 2],
+
+    queue_avail_low: WO<u32>,
+    queue_avail_high: WO<u32>,
+
+    /// Reserved
+    __r8: [u32; 2],
+
+    queue_used_low: WO<u32>,
+    queue_used_high: WO<u32>,
+
+    /// Reserved
+    __r9: [u32; 21],
+
+    config_generation: RO<u32>,
+}
+
+bitflags::bitflags! {
+    /// The device status field.
+    struct DeviceStatus: u32 {
+        /// Indicates that the guest OS has found the device and recognized it
+        /// as a valid virtio device.
+        const ACKNOWLEDGE = 1;
+
+        /// Indicates that the guest OS knows how to drive the device.
+        const DRIVER = 2;
+
+        /// Indicates that something went wrong in the guest, and it has given
+        /// up on the device. This could be an internal error, or the driver
+        /// didn’t like the device for some reason, or even a fatal error
+        /// during device operation.
+        const FAILED = 128;
+
+        /// Indicates that the driver has acknowledged all the features it
+        /// understands, and feature negotiation is complete.
+        const FEATURES_OK = 8;
+
+        /// Indicates that the driver is set up and ready to drive the device.
+        const DRIVER_OK = 4;
+
+        /// Indicates that the device has experienced an error from which it
+        /// can’t recover.
+        const DEVICE_NEEDS_RESET = 64;
+    }
+}
